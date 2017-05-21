2ddc419c37eb3f643c90426b8e9820b8475be76f
van chung chen <auxo_86@hotmail.com>
2017-05-21 14:48:31 +0800

    加入專案檔案。

---
+++ module_04_071/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace module_04_071
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="subject 不可為空白")]
+        public string subject { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string HtmlComment { get; set; }
+        public int Price { get; set; }
+        public bool IsApprove { get; set; }
+    }
+}
