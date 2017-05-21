62bb050757cb5f6f9e4735997dcd76b8f44b4607
van chung chen <auxo_86@hotmail.com>
2017-05-21 14:52:10 +0800

    使用 DataAnnotations 屬性驗證模型資料

--- module_04_071/Comment.cs
+++ module_04_071/Comment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//使用 DataAnnotations 屬性驗證模型資料
 using System.ComponentModel.DataAnnotations;
 
 namespace module_04_071
